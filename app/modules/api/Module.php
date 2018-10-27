e0346d766b745678658b54fdf5d9f6eede529d59
Ahmed Saad <ask@shopbox.com>
2018-10-27 14:05:12 +0200

    api module config class

---
+++ app/modules/api/Module.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\modules\api;
+
+use Yii;
+
+/**
+ * api module definition class
+ */
+class Module extends yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\api\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        // initialize the module with the configuration loaded from config.php
+
+    }
+}
