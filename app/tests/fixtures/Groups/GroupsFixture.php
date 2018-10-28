670ecea9104f0df4bdf2d530039604e1460363b1
Ahmed Saad <ask@shopbox.com>
2018-10-28 02:04:11 +0200

    fixtures

---
+++ app/tests/fixtures/Groups/GroupsFixture.php
@@ -0,0 +1,9 @@
+<?php
+namespace tests\fixtures\Groups;
+
+use yii\test\ActiveFixture;
+
+class GroupsFixture extends ActiveFixture
+{
+    public $modelClass = 'app\modules\api\models\Groups';
+}
---
+++ app/tests/fixtures/Users/UsersFixture.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\tests\fixtures\Users;
+
+use yii\test\ActiveFixture;
+
+class UsersFixture extends ActiveFixture
+{
+    public $modelClass = 'app\modules\api\models\Users';
+}
